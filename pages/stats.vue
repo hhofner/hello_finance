2159b0ece759a35b4a0c8e01836f1499364059b2
Hans Hofner <hhofner@gmail.com>
2024-08-11 20:45:56 +0900

    add data lib and pinia

--- pages/stats.vue
+++ pages/stats.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import { VisDonut, VisSingleContainer, VisTooltip } from '@unovis/vue'
+import { Donut } from '@unovis/ts'
+
+const props = defineProps<{ data: number[] }>()
+const triggers = { [Donut.selectors.segment]: d => d.data }
+const value = (d: number) => d
+</script>
+
 <template>
-  <div />
+  <VisSingleContainer :data="data">
+    <VisTooltip :triggers="triggers" />
+    <VisDonut :value="value" />
+  </VisSingleContainer>
 </template>
